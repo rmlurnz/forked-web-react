7d7e4ab3f9c3eabf58c77a3adff3b43c3c0a3d79
DESKTOP-KC0AM8L\Ryan <ryanmlerner@gmail.com>
2024-04-04 15:37:30 -0600

    add auth context, login + signup routes / logic

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RecipesContextProvider } from './context/RecipeContext'
+import { AuthContextProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RecipesContextProvider>
-      <App />
-    </RecipesContextProvider>
+    <AuthContextProvider>
+      <RecipesContextProvider>
+        <App />
+      </RecipesContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
 )
